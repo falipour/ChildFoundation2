4d01bcd1932070373628c01db42261d226d7843a
Fatemeh <fatemehalpr@gmail.com>
2018-02-01 10:49:02 +0330

    migrating finished :)

--- ChildFoundation2/settings.py
+++ ChildFoundation2/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'p-wm20h5qhn$%obuj18c41+&!gvquy)1)6c=b4eb%md46if484'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ChildFoundation2.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -105,13 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Iran'
 
 USE_I18N = True
 
@@ -119,10 +114,21 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '',
+]
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+# TODO FATEMEH
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'fg75527@gmail.com'
+# EMAIL_HOST_PASSWORD = 'fg75527_fg75527'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+#
+# ADMINS = [('FG', 'f.gorbanpor@yahoo.com')]
--- MySite/views.py
+++ MySite/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from .forms import ContactForm
 from karbar.models import MyUser
-# from hamyar.models import Hamyar
+from hamyar.models import Hamyar
 from karbar.forms import SignupForm1
 from django.contrib.auth import authenticate, login as auth_login
 
@@ -49,34 +49,33 @@ class ContactView(TemplateView):
         return render(request, self.template_name, args)
 
 
-# TODO FATEMEH
-# def signup(request):
-#     form = SignupForm1(request.POST)
-#     phone_number = request.POST.get('phone_number')
-#     country = request.POST.get('report_method')
-#     city = request.POST.get('city')
-#     address = request.POST.get('address')
-#     postal_code = request.POST.get('postal_code')
-#     report_method = request.POST.get('report_method')
-#     context = {'phone_number': phone_number, 'report_method': report_method, 'country': country, 'city': city,
-#                'postal_code': postal_code, 'address': address}
-#     if form.is_valid():
-#         if len(phone_number) == 11 and phone_number[0:2] == '09':
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             auth_login(request, user)
-#             member = MyUser.objects.create(user=user, phone_number=phone_number, country=country, city=city,
-#                                            postal_code=postal_code, address=address)
-#             member.save()
-#             manager = Hamyar.objects.create(member=member, report_method=report_method)
-#             manager.save()
-#             return render(request, 'Hamyar/Home-Hamyar.html', {'type': 'complexRegister'})
-#         else:
-#             phone_number_error = "شماره تلفن باید 11 رقمی باشد و با 09 آغاز شود."
-#             context['phoneNumberError'] = phone_number_error
-#
-#     context['form'] = form
-#     context['type'] = "signup"
-#     return render(request, 'MySite/Hamyar_Register.html', context)
+def signup(request):
+    form = SignupForm1(request.POST)
+    phone_number = request.POST.get('phone_number')
+    country = request.POST.get('report_method')
+    city = request.POST.get('city')
+    address = request.POST.get('address')
+    postal_code = request.POST.get('postal_code')
+    report_method = request.POST.get('report_method')
+    context = {'phone_number': phone_number, 'report_method': report_method, 'country': country, 'city': city,
+               'postal_code': postal_code, 'address': address}
+    if form.is_valid():
+        if len(phone_number) == 11 and phone_number[0:2] == '09':
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            auth_login(request, user)
+            member = MyUser.objects.create(user=user, phone_number=phone_number, country=country, city=city,
+                                           postal_code=postal_code, address=address)
+            member.save()
+            manager = Hamyar.objects.create(member=member, report_method=report_method)
+            manager.save()
+            return render(request, 'Hamyar/Home-Hamyar.html', {'type': 'complexRegister'})
+        else:
+            phone_number_error = "شماره تلفن باید 11 رقمی باشد و با 09 آغاز شود."
+            context['phoneNumberError'] = phone_number_error
+
+    context['form'] = form
+    context['type'] = "signup"
+    return render(request, 'MySite/Hamyar_Register.html', context)
